c7ce9751376556ab48adf2dff86e02e84fd52e9a
dhinagarravi <107559258+dhinagarravi@users.noreply.github.com>
2023-02-06 08:08:32 +0530

    first commit

--- src/app/middleware/middleware.js
+++ src/app/middleware/middleware.js
@@ -4,13 +4,18 @@ const USER_SERVICE = require('../services/user.services');
 
 // Authenticate user
 const authenticate = async (req, res, next) => {
-  const token = req.header("x-auth-token");
-  if (!token) return res.status(401).send("Access denied. No token provided.");
+  const authHeader = req.header('Authorization');
+  const userId = req.header('userId');
+  if (!authHeader || !userId) {
+    return res.status(401).send("Access denied. No token provided.");
+  }
+  const token = authHeader.split(' ')[1];
+  if (!token||token!==' ') return res.status(401).send("Access denied. No token provided.");
 
   try {
-    const USER = await USER_SERVICE.loginUser(req.params.userId)
+    const USER = await USER_SERVICE.loginUser(userId)
     if (!USER) return res.status(401).send("un Authorized");
-    const decoded = jwt.verify(token, "jwtPrivateKey");
+    const decoded = jwt.verify(token, "jwtPrivateKey"); // this peivate key should be come from config file.
     req.user = decoded;
     next();
   } catch (ex) {
